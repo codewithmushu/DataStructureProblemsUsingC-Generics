a33649d5024a2a1a7b044294d25b5da1267f8d75
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-03-29 00:20:43 +0530

    [Muskan] Added : Main Method.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_structure
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+
+            Console.WriteLine("Peek: " + stack.Peek());
+
+            while (!stack.IsEmpty())
+            {
+                Console.WriteLine(stack.Pop());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
