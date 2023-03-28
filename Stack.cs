25306cfbbe27349e5239d3ace5dbb5e7e8f503dd
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-03-29 00:24:20 +0530

    [Muskan] Added : Ability to create a Stack of 56->30->70 - Use LinkedList to do the Stack Operations - Here push will internally call add method on LinkedList. - So 70 will be added first then 30 and then 56 to make 56 on top of the Stack

---
+++ Stack.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_structure
+{
+    class Stack<T>
+    {
+        private LinkedList<T> list;
+
+        public Stack()
+        {
+            list = new LinkedList<T>();
+        }
+
+        public void Push(T item)
+        {
+            list.AddFirst(item);
+        }
+
+        public T Pop()
+        {
+            if (list.First == null)
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+
+            T item = list.First.Value;
+            list.RemoveFirst();
+            return item;
+        }
+
+        public T Peek()
+        {
+            if (list.First == null)
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+
+            return list.First.Value;
+        }
+
+        public bool IsEmpty()
+        {
+            return list.First == null;
+        }
+    }
+}
