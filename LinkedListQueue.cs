730c3cb28e32edf0f6a4f451047e597257b0cda2
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-03-29 16:33:34 +0530

    [Muskan] Added : [200~Ability to create a Queue of

---
+++ LinkedListQueue.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueProblem
+{
+    public class LinkedListQueue<T> : IEnumerable<T>
+    {
+        private LinkedListNode<T> head;
+        private LinkedListNode<T> tail;
+
+        public int Count { get; private set; }
+
+        public void Enqueue(T value)
+        {
+            var newNode = new LinkedListNode<T>(value);
+            if (tail == null)
+            {
+                head = tail = newNode;
+            }
+            else
+            {
+                tail.Next = newNode;
+                tail = newNode;
+            }
+            Count++;
+        }
+
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var current = head;
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+
+
+}
