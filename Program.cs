6171b683fe77c2e2b1ffc254e0b6b6802a713985
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-03-29 16:28:02 +0530

    [Muskan] Added : created instance of queue and adding enqueue.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var queue = new LinkedListQueue<int>();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+
+            Console.WriteLine("Initial queue:");
+            PrintQueue(queue);
+
+            
+        }
+
+        static void PrintQueue(LinkedListQueue<int> queue)
+        {
+            foreach (var item in queue)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
